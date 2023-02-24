e5dd6b738f6971fd74a67fb92c6bcedb56ded5c4
Cesar Soares <cesarpacco2@hotmail.com>
2023-02-24 11:21:34 -0300

    Projeto Terraform

---
+++ modules/network/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc" {
+  value = aws_vpc.my_vpc
+
+}
+
+output "subnet_private" {
+  value = aws_subnet.private
+ 
+}
+
+output "subnet_public_id" {
+  value = values(aws_subnet.public).*.id
+
+}
